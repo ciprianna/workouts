b1e7a22e0ef6172229dab7280ebba07eaf20fd79
Ciprianna <ciprianna17@gmail.com>
2015-09-01 16:58:22 -0500

    Created methods for exercises in the controller.

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,6 +1,31 @@
 class ExercisesController < ApplicationController
 
+  # Exercise homepage - evaluate if needed
+  def index
+  end
+
+  # Display form to create a new exercise
+  def create
+  end
+
+  # Create the new exercise
   def new
   end
-  
+
+  # Display a form to edit an exercise
+  def edit
+  end
+
+  # Show information about an exercise
+  def show
+  end
+
+  # Update an exercise (save)
+  def update
+  end
+
+  # Delete an exercise
+  def delete
+  end
+
 end
